9d0bc2cc68249a3984e910d4b1f6edfd5c376e7d
Meow Roar <sareacct91@gmail.com>
2024-02-28 14:49:47 -0800

    npm setup

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./views/**/*.hbs"],
+  theme: {
+    extend: {
+      colors: {
+        'dark': '#231010',
+        'light': '#d9ead3'
+      }
+    },
+  },
+  plugins: [],
+}
