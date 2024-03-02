a9002f76e0c1817b30321a3c5d2801c94d67d87b
Meow Roar <sareacct91@gmail.com>
2024-03-02 10:06:47 -0800

    added blogRoutes

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ const services = require('../services');
 const userAuth = require('../utils/auth');
 
 const userRoutes = require('./api/userRoutes');
+const blogRoutes = require('./api/blogRoutes');
 
 router.route('/')
   .get(services.renderHome);
@@ -18,7 +19,7 @@ router.route('/logout')
   .post(services.userLogout);
 
 router.use('/users', userRoutes);
-
+router.use('/blogs', userAuth, blogRoutes);
 
 
 module.exports = router;
