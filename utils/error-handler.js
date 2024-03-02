e02f1c98a3a735777dcc1e3e76e3dccdc5cc2452
Meow Roar <sareacct91@gmail.com>
2024-03-02 06:49:06 -0800

    added more error handling

--- utils/error-handler.js
+++ utils/error-handler.js
@@ -1,11 +1,20 @@
-function errorHandler(err, req, res, next) {
-  console.log('\nerror-handler\n');
-  console.log(err);
+const { ValidationError } = require('sequelize');
+const { Blog, User, Comment } = require('../model');
 
+function errorHandler(err, req, res, next) {
+  console.log('\n\nerror-handler\n\n');
+  console.log('err:', err);
+
+  const errInstance = err.errors[0].instance || null;
   const customErr = {
     code: err.code || 500,
     msg: err.message || 'Something went wrong. Try again later',
   };
+
+  if (err instanceof ValidationError && errInstance instanceof User) {
+    customErr.code = 400;
+    customErr.msg = 'Something went wrong, try another name'
+  }
 
   return res.status(customErr.code).json({ msg: customErr.msg });
 };
