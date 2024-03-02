144e531c6a36952b462bb5cd4046878bbbcbcf40
Meow Roar <sareacct91@gmail.com>
2024-03-02 10:08:22 -0800

    seeded comment to db

--- seeds/index.js
+++ seeds/index.js
@@ -1,15 +1,15 @@
+const sequelize = require('../config/connection');
 const { User, Blog, Comment } = require('../model');
 const userData = require('./userData.json');
 const blogData = require('./blogData.json');
-const sequelize = require('../config/connection');
-
+const commentData = require('./commentData.json');
 
 (async function () {
   await sequelize.sync({ force: true });
 
   await User.bulkCreate(userData);
-
   await Blog.bulkCreate(blogData);
+  await Comment.bulkCreate(commentData);
 
   process.exit(0);
 })();
