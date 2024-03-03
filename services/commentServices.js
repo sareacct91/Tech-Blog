b521c949b465149cae2429b7f126058e3658993d
Meow Roar <sareacct91@gmail.com>
2024-03-03 03:07:16 -0800

    added comment services

---
+++ services/commentServices.js
@@ -0,0 +1,25 @@
+const { BadRequestError } = require('../utils/errors');
+const { createOneComment } = require('../utils/queries/comment');
+
+
+async function createComment(req, res) {
+  const { content, blog_id } = req.body;
+  const { user_id } = req.session;
+
+  const inputData = {
+    content,
+    user_id,
+    blog_id
+  };
+
+  const comment = await createOneComment(inputData);
+  const commentData = comment.toJSON();
+
+  res.status(201).json({status: 'created', data: commentData})
+}
+
+
+module.exports = {
+  createComment,
+
+}
