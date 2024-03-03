3a62caf8b3c416a9b62034957c852b98c40d8b73
Meow Roar <sareacct91@gmail.com>
2024-03-03 05:27:53 -0800

    move routes to renderRoutes and services

--- routes/index.js
+++ routes/index.js
@@ -1,23 +1,8 @@
 const router = require('express').Router();
-const services = require('../services');
-const userAuth = require('../utils/auth');
+const renderRoutes = require('./renderRoutes');
 const apiRoutes = require('./api');
 
-
-router.route('/')
-  .get(services.renderHome);
-
-router.route('/signup')
-  .get(services.renderSignup)
-
-router.route('/login')
-  .get(services.renderLogin)
-  .post(services.userLogin);
-
-router.route('/logout')
-  .post(services.userLogout);
-
+router.use('/', renderRoutes);
 router.use('/api', apiRoutes);
-
 
 module.exports = router;
